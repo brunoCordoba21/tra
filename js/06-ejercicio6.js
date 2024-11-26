ed598ea62f200b2d9329289ebb91d6cdd10546a4
Bruno Cordoba <cordobabruno32@gmail.com>
2024-11-25 22:23:10 -0300

    Se realizo el ejercicio6 de Js

--- js/06-ejercicio6.js
+++ js/06-ejercicio6.js
@@ -6,3 +6,11 @@ input: 15 , 3
 
 Output: El 15 es el número más grande
  */
+
+let num1 = parseInt(prompt('Ingresar un numero'))
+let num2 = parseInt(prompt('Ingresar otro numero'))
+if(num1 > num2){
+    document.write(`El numero ${num1} es mayor al ${num2}`)
+}else{
+    document.write(`El numero ${num2} es mayor al numero ${num1}`)
+}
