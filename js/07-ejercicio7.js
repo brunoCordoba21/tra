b8ff12123a8500be4bee6437bc355cb8a6f111c7
Bruno Cordoba <cordobabruno32@gmail.com>
2024-11-27 00:42:25 -0300

    Se modifico el ejercicio, se cambio la estructura del condicional por el else if

--- js/07-ejercicio7.js
+++ js/07-ejercicio7.js
@@ -9,17 +9,23 @@ Output: El 15 es el número más grande
 
 let numero1 = parseInt(prompt('Ingresar número 1'))
 let numero2 = parseInt(prompt('Ingresar número 2'))
-let numero3 =parseInt(prompt('Ingresar número 3'))
+let numero3 = parseInt(prompt('Ingresar número 3'))
 
-if(numero1 > numero2 && numero1 > numero3){
-    document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el primero que se ingreso que equivale a ${numero1}  `)
+// if (numero1 > numero2 && numero1 > numero3) {
+//     document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el primero que se ingreso que equivale a ${numero1}  `)
+// } else {
+//     if (numero2 > numero1 && numero2 > numero3) {
+//         document.write(`El mayor de los números  ${numero1}, ${numero2} y ${numero3} es el segundo que se ingreso que equivale a ${numero2}`)
+
+//     } else {
+//         document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el tercero que se ingreso que equivale a ${numero3}`)
+//     }
+
+if (numero1 > numero2 && numero1 > numero3) {
+   document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el primero que se ingreso que equivale a ${numero1}  `)
+
+}else if(numero2 > numero1 && numero2 > numero3){
+    document.write(`El mayor de los números  ${numero1}, ${numero2} y ${numero3} es el segundo que se ingreso que equivale a ${numero2}`)
 }else{
-    if(numero2 > numero1 && numero2 > numero3){
-   document.write(`El mayor de los números  ${numero1}, ${numero2} y ${numero3} es el segundo que se ingreso que equivale a ${numero2}`)
-
-    }else{
-        document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el tercero que se ingreso que equivale a ${numero3}`)
-    }
+    document.write(`El mayor de los números ${numero1}, ${numero2} y ${numero3} es el tercero que se ingreso que equivale a ${numero3}`)
 }
-
-
